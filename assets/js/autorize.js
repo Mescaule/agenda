6104fd120fd31df82db6db5f50ad2d8eecab0959
Makawlly Rocha <makawllyrr1@gmail.com>
2024-02-14 14:41:24 -0300

    Projeto Agenda

---
+++ assets/js/autorize.js
@@ -0,0 +1,16 @@
+// localStorage.setItem('token', '123');
+// // Para armazenar um item no Local Storage
+
+// // Para buscar um item no Local Storage
+// var item = localStorage.getItem('token');
+
+// // Verificar se o item existe
+// if (item !== '1234') {
+
+//     window.location.href = 'index.html';
+
+// } else {
+
+//     window.location.href = 'agenda.html';
+
+// }
