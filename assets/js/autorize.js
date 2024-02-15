10627297d13ec99c4e71c2e3dae361ae5d12ebbf
Makawlly Rocha <makawllyrr1@gmail.com>
2024-02-15 13:57:40 -0300

    Tela de login + assets

--- assets/js/autorize.js
+++ assets/js/autorize.js
@@ -14,3 +14,24 @@
 //     window.location.href = 'agenda.html';
 
 // }
+
+const form = document.getElementById('form')
+const inputName = document.getElementById('name').value
+const inputEmail = document.getElementById('email').value
+const botao = document.getElementById('btn')
+
+async function enviaDados(e) {
+    e.preventDefault()
+    console.log('teste');
+    const user = {
+        name: inputName, 
+        email: inputEmail
+    }
+    await fetch('https://2a99-131-0-91-21.ngrok-free.app/user', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(user)
+    })
+}
+
+form.addEventListener('submit', enviaDados)
